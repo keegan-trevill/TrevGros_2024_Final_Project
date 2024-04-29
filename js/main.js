a2f06892498dc1139e5676037fc5f19d0771f3e1
keegan-trevill <157654717+keegan-trevill@users.noreply.github.com>
2024-04-29 12:59:51 -0500

    Asylum and refugee menu

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 //declare map variable globally so all functions have access
 var map;
 var minValue;
+var geojsonLayer;
 var dataStats = {}; 
 var dataStatsUS= {};
 //create an attributes array
-var attributes = ["Refugee", "Asylum"];
+var attributes = ["Refugees", "Asylum"];
+
+
+
 //step 1 create map
 function createMap(){
     //create the map
@@ -23,13 +27,14 @@ function createMap(){
     //call getData function
     getData(map);
 };
-function processData(data) {
-    var attributes = [];
+function processDataRef(data) {
+   var attributes = [];
 
     data.features.forEach(feature => {
         var properties = feature.properties;
         for (var key in properties) {
-            if (key.startsWith('Refugees') || key.startsWith('total') && !attributes.includes(key)) {
+            if (key.startsWith('Refugees') 
+            && !attributes.includes(key)) {
                 attributes.push(key);
             }
         }
@@ -38,23 +43,41 @@ function processData(data) {
     console.log(attributes);
     return attributes;
 }
+
+function processDataAsylum(data) {
+    var attributes = [];
+
+    data.features.forEach(feature => {
+        var properties = feature.properties;
+        for (var key in properties) {
+            if (key.startsWith('Asylum') 
+            && !attributes.includes(key)) {
+                attributes.push(key);
+            }
+        }
+    });
+
+    console.log(attributes);
+    return attributes;
+}
+
 function processDataUS(data) {
     var attributes = [];
 
     data.features.forEach(feature => {
         var properties = feature.properties;
         for (var key in properties) {
-            if (key.startsWith('Refugees') || key.startsWith('total') && !attributes.includes(key)) {
+             if (key.startsWith('total') && !attributes.includes(key)) {
                 attributes.push(key);
+             }
             }
-        }
     });
 
     console.log(attributes);
     return attributes;
 }
 
-function calcStats(data){
+function calcStatsRef(data){
     //create empty array to store all data values
     var features = data.features;
     var dataStats = {};
@@ -111,6 +134,65 @@ var averageRefugees = totalRefugees / features.length;
 });    
 
 }
+
+function calcStatsAsylum(data){
+    //create empty array to store all data values
+    var features = data.features;
+    var dataStats = {};
+    var minAsylum = Infinity;
+    var maxAsylum = -Infinity;
+    var totalAsylum = 0;
+    // Looping through each feature
+    features.forEach(function(feature) {
+        // Accessing properties of each feature
+        var properties = feature.properties;
+        // Now you can access individual properties of each feature
+        // For example:
+        var name = properties.Country;
+        var asylum = properties.Asylum;
+        console.log("Asylum:" + asylum + ", " + "Country: " + name)
+    var allValues = [];
+    
+    if (asylum < minAsylum) {
+        minAsylum = asylum;
+    }
+    if (asylum > maxAsylum) {
+        maxAsylum = asylum;
+    } 
+    totalAsylum += asylum;
+
+
+// Calculate average
+var averageAsylum = totalAsylum / features.length;
+
+    //loop through each city
+    for(var city of data.features){
+        //loop through each year
+       // for(var year = 2021; year <= 2021; year+=1){
+              //get population for current year
+              var value = city.properties["Asylum"];
+              //add value to array
+              allValues.push(value);
+        //}
+    }
+    //get min, max, mean stats for our array
+    //dataStats.min = Math.min(...allValues);
+    //dataStats.max = Math.max(...allValues);
+    //calculate meanValue
+    //var sum = allValues.reduce(function(a, b){return a+b;});
+    //dataStats.mean = sum/ allValues.length;
+    dataStats.min = 0;
+    dataStats.max = 43964;
+    dataStats.mean = averageAsylum
+    console.log("Minimum:", minAsylum);
+    console.log("Maximum:", maxAsylum);
+    console.log("Mean:", averageAsylum);
+    return dataStats;
+
+});    
+
+}
+
 function calcStatsUS(data){
     //create empty array to store all data values
     var features = data.features;
@@ -172,18 +254,7 @@ var average = total / features.length;
 
 function getData(map){
     //load the data
-    fetch("data/2021WorldData.geojson")
-        .then(function(response){
-            return response.json();
-        })
-        .then(function(json){
-             //create an attributes array
-            var attributes = processData(json);
-            calcStats(json); 
-            createPropSymbols(json, attributes);
-            //createSequenceControls(attributes);
-             
-        })
+    
     fetch("data/UsDataSIV.geojson")
     .then(function(response){
         return response.json();
@@ -195,15 +266,73 @@ function getData(map){
        createPropSymbolsUS(json, attributesUS);
        //createSequenceControlsUS(attributesUS);
     })
+    fetch("data/geoBoundaries-AFG-ADM0_simplified.geojson")
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        L.geoJSON(data).addTo(map);
+    })
 };
+
+// Define a function to handle the change event of the dropdown menu
+function dropdownChange() {
+    // Get the selected attribute from the dropdown menu
+    var selectedAttribute = document.getElementById("attributeDropdown").value;
+    console.log(selectedAttribute)
+    // Fetch the GeoJSON data
+    fetch("data/2021WorldData.geojson")
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(json) {
+            // Process the data based on the selected attribute
+            var attributes;
+            if (selectedAttribute === "Refugees") {
+                attributes = processDataRef(json);
+                calcStatsRef(json);
+                
+            } else if (selectedAttribute === "Asylum") {
+                attributes = processDataAsylum(json);
+                calcStatsAsylum(json);
+                
+            }
+
+            // If a previous GeoJSON layer exists, remove it from the map
+            if (geojsonLayer) {
+                map.removeLayer(geojsonLayer);
+            }
+
+            // Create the new GeoJSON layer based on the selected attribute
+            geojsonLayer = createPropSymbols(json, attributes);
+        });
+}
+document.getElementById("attributeDropdown").addEventListener("change", dropdownChange);
+
 
 //calculate the radius of each proportional symbol
 function calcPropRadius(attValue) {
     // Define a maximum radius to cap the symbol sizes
     var maxRadius = 30; // You can adjust this value based on your preference
-
+    console.log(attValue)
     // Scale the attribute value to fit within a reasonable range
     var scaledValue = attValue / 1490562; // Assuming you have maxValue calculated elsewhere
+
+    // Cap the scaled value to ensure it doesn't exceed 1
+    scaledValue = Math.min(scaledValue, 1);
+
+    // Calculate the radius using a linear interpolation
+    var radius = scaledValue * maxRadius;
+
+    return radius;
+}
+
+function calcPropRadiusAsylum(attValue) {
+    // Define a maximum radius to cap the symbol sizes
+    var maxRadius = 30; // You can adjust this value based on your preference
+    console.log(attValue)
+    // Scale the attribute value to fit within a reasonable range
+    var scaledValue = attValue / 43964; // Assuming you have maxValue calculated elsewhere
 
     // Cap the scaled value to ensure it doesn't exceed 1
     scaledValue = Math.min(scaledValue, 1);
@@ -230,14 +359,12 @@ function calcPropRadiusUS(attValue) {
     return radius;
 }
 
-
-
-function pointToLayer(feature, latlng, attributes){
+function pointToLayerAsylum(feature, latlng, attributes){
     //Determine which attribute to visualize with proportional symbols
         //Step 4: Assign the current attribute based on the first index of the attributes array
-        var attribute = attributes[0];
+        var attribute = attributes[1];
         //check
-        console.log(attribute);
+        console.log(feature);
     //create marker options
     var options = {
         fillColor: "#ff7800",
@@ -254,6 +381,46 @@ function pointToLayer(feature, latlng, attributes){
     //Give each feature's circle marker a radius based on its attribute value
     options.radius = calcPropRadius(attValue);
 
+    //create circle marker layer
+    var layer = L.circleMarker(latlng, options);
+
+    var popupContent = new PopupContentWorld(feature.properties, attribute);
+
+    //bind the popup to the circle marker    
+    layer.bindPopup(popupContent.formatted, { 
+        offset: new L.Point(0,-options.radius)
+    });
+
+
+    //return the circle marker to the L.geoJson pointToLayer option
+    return layer;
+};
+
+function pointToLayer(feature, latlng, attributes){
+    //Determine which attribute to visualize with proportional symbols
+        //Step 4: Assign the current attribute based on the first index of the attributes array
+        var attribute = attributes[0];
+        //check
+        console.log(attributes[0]);
+    //create marker options
+    var options = {
+        fillColor: "#ff7800",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+        
+    };
+    
+    //For each feature, determine its value for the selected attribute
+    var attValue = Number(feature.properties[attribute]);
+
+    //Give each feature's circle marker a radius based on its attribute value
+    if(attributes[0] == 'Asylum'){
+        options.radius = calcPropRadiusAsylum(attValue);
+    }else if(attributes[0] == 'Refugees'){
+    options.radius = calcPropRadius(attValue);
+    }
     //create circle marker layer
     var layer = L.circleMarker(latlng, options);
 
@@ -312,10 +479,13 @@ function createPropSymbols(data, attributes){
     //create a Leaflet GeoJSON layer and add it to the map
     L.geoJson(data, {
         pointToLayer: function(feature, latlng){
+            
             return pointToLayer(feature, latlng, attributes);
+            
         }
     }).addTo(map);
 };
+
 function createPropSymbolsUS(data, attributes){
     //create a Leaflet GeoJSON layer and add it to the map
     L.geoJson(data, {
@@ -331,12 +501,19 @@ function PopupContentUS(properties, attribute){
     this.sIV = this.properties[attribute];
     this.formatted = "<p><b>State:</b> " + this.properties.state + "</p><p><b>Total SIV refugees: " + ":</b> " + this.sIV + " </p>";
 };
-function PopupContentWorld(properties, attribute){
+function PopupContentWorld(properties, attribute) {
     this.properties = properties;
     this.attribute = attribute;
-    this.refugee = this.properties[attribute];
-    this.formatted = "<p><b>Country: </b> " + this.properties.Country + "</p><p><b>Total refugees" + ": </b> " + this.refugee + " </p>";
-};
+    
+    // Check if the attribute is "Refugees" or "Asylum" and set the corresponding property
+    if (attribute === 'Refugees') {
+        this.refugees = this.properties[attribute];
+        this.formatted = "<p><b>Country: </b> " + this.properties.Country + "</p><p><b>Total refugees: </b> " + this.refugees + " </p>";
+    } else if (attribute === 'Asylum') {
+        this.asylum = this.properties[attribute];
+        this.formatted = "<p><b>Country: </b> " + this.properties.Country + "</p><p><b>Total asylum seekers: </b> " + this.asylum + " </p>";
+    }
+}
 
 document.getElementById("zoomButton").addEventListener("click", zoomToPakistanAndIran);
 
@@ -347,6 +524,8 @@ function zoomToPakistanAndIran() {
         [24.0, 60.0], // Southwest coordinates (Pakistan)
         [39.0, 77.0]  // Northeast coordinates (Iran)
     ];
+    var narrativeContent = "Zoomed to surrounding countries.";
+    openNarrativeBox(narrativeContent);
 
     // Fit the map view to the bounds of Pakistan and Iran
     map.fitBounds(pakistanIranBounds);
@@ -364,10 +543,11 @@ document.getElementById("zoomToGermanyButton").addEventListener("click", zoomToG
 function zoomToGermany() {
     // Set the coordinates and zoom level for Germany
     var germanyBounds = [
-        [47.2, 5.8], // Southwest coordinates
-        [55.2, 15.2] // Northeast coordinates
+        [39.7986609345133, -9.31640625], // Southwest coordinates
+        [61.60639637138628, 27.421875] // Northeast coordinates
     ];
-
+    var narrativeContent = "Zoomed to Germany.";
+    openNarrativeBox(narrativeContent);
     // Fit the map view to the bounds of Germany
     map.fitBounds(germanyBounds);
 }
@@ -382,9 +562,30 @@ function zoomToUSA() {
         [24.396308, -125.0], // Southwest coordinates
         [49.384358, -66.93457] // Northeast coordinates
     ];
+    var narrativeContent = "Zoomed to the USA.";
 
+    // Open the narrative box with the specified content
+    openNarrativeBox(narrativeContent);
     // Fit the map view to the bounds of the USA
     map.fitBounds(USBounds);
+}
+// Function to open the narrative box
+function openNarrativeBox(content) {
+    var narrativeBox = document.getElementById('narrativeBox');
+    var narrativeText = document.getElementById('narrativeText');
+    
+    // Set the content of the narrative box
+    narrativeText.innerHTML = content;
+
+    // Show the narrative box
+    narrativeBox.style.display = 'block';
+}
+
+// Function to close the narrative box
+function closeNarrativeBox() {
+    var narrativeBox = document.getElementById('narrativeBox');
+    // Hide the narrative box
+    narrativeBox.style.display = 'none';
 }
 
 document.addEventListener('DOMContentLoaded',createMap)
